9a34a2f8050c4c4a034327b4baf7eaecdc561ef8
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-01-28 15:56:53 +0100

    Better color management and checkbox styles

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,38 +1,36 @@
-const stylus = require('stylus')
-const {readFileSync, writeFileSync} = require('fs')
+const stylus = require("stylus");
+const { readFileSync, writeFileSync } = require("fs");
 
 function runStylus(name, done, force = false) {
-  const expectedPath = __dirname + '/' + name + '-expected.css'
-  let template = readFileSync(__dirname + '/' + name + '.styl', 'utf8')
-  let expected = null
+  const expectedPath = __dirname + "/" + name + "-expected.css";
+  let template = readFileSync(__dirname + "/" + name + ".styl", "utf8");
+  let expected = null;
   try {
-    expected = readFileSync(expectedPath, 'utf8')
+    expected = readFileSync(expectedPath, "utf8");
   } catch (err) {
-    console.error('Exception:', err)
+    console.error("Exception:", err);
   }
-  stylus.render(template, {paths: [__dirname]}, function (err, css) {
-    if (err) throw err
+  stylus.render(template, { paths: [__dirname] }, function (err, css) {
+    if (err) throw err;
     if (force || expected == null) {
-      writeFileSync(expectedPath, css, 'utf8')
-      console.log(css)
+      writeFileSync(expectedPath, css, "utf8");
+      console.log(css);
     }
-    expect(css).toEqual(expected)
-    done()
-  })
+    expect(css).toEqual(expected);
+    done();
+  });
 }
 
-describe('twindy', () => {
+describe("twindy", () => {
+  it("should be empty", (done) => {
+    runStylus("empty", done);
+  });
 
-  it('should be empty', done => {
-    runStylus('empty', done)
-  })
+  it("should reset", (done) => {
+    runStylus("reset", done);
+  });
 
-  it('should reset', done => {
-    runStylus('reset', done)
-  })
-
-  it('should units', done => {
-    runStylus('units', done)
-  })
-
-})
+  it("should units", (done) => {
+    runStylus("units", done);
+  });
+});
