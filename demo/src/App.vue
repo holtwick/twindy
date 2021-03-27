ec1f00f72718117e4df6a25aea08cec68623658e
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-03-27 21:41:59 +0100

    Simple navigation

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="header">
-    <select class="dropdown">
-      <option>Prose</option>
+    <select class="dropdown" v-model="componentName">
+      <option v-for="name in names" :key="name">{{ name }}</option>
     </select>
   </div>
   <div class="indent">
-    <prose />
+    <component :is="components[componentName]" />
   </div>
 </template>
 
 <script setup lang="ts">
-import prose from "./components/prose.vue"
+import { useSessionStorage } from "@vueuse/core"
+import { defineAsyncComponent, reactive, ref } from "vue"
+
+const componentName = useSessionStorage("componentName", "")
+const component = ref(null)
+
+// @ts-ignore
+let modules = import.meta.glob("./components/*.vue")
+
+let components: any = {}
+
+let names = Object.entries(modules).map(([path, component]) => {
+  const name = path.split("/").pop()?.slice(0, -4)
+  // @ts-ignore
+  components[name] = defineAsyncComponent(component)
+  return name
+})
+
+names.sort()
+
+// console.log(components)
+// const prose = defineAsyncComponent(() => import("./components/prose.vue"))
 </script>
---
+++ demo/src/components/form.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="prose">form</div>
+</template>
+
+<script setup lang="ts"></script>
