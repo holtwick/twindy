3400b0e056764b9921a45c489ea494d777abee17
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-03-27 13:21:59 +0100

    Start a demo

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,39 +1,27 @@
-const stylus = require("stylus");
-const { readFileSync, writeFileSync } = require("fs");
+const stylus = require("stylus")
+const { readFileSync } = require("fs")
 
 function runStylus(name, done, force = false) {
-  // const expectedPath = __dirname + "/" + name + "-expected.css";
-  let template = readFileSync(__dirname + "/" + name + ".styl", "utf8");
-  // let expected = null;
-  // try {
-  //   expected = readFileSync(expectedPath, "utf8");
-  // } catch (err) {
-  //   console.error("Exception:", err);
-  // }
+  let template = readFileSync(__dirname + "/" + name + ".styl", "utf8")
   stylus.render(template, { paths: [__dirname] }, function (err, css) {
-    if (err) throw err;
-    // if (force || expected == null) {
-    //   writeFileSync(expectedPath, css, "utf8");
-    //   console.log(css);
-    // }
-
+    if (err) throw err
 
     // https://jestjs.io/docs/en/snapshot-testing
-    expect(css).toMatchSnapshot();
-    done();
-  });
+    expect(css).toMatchSnapshot()
+    done()
+  })
 }
 
 describe("twindy", () => {
   it("should be empty", (done) => {
-    runStylus("empty", done);
-  });
+    runStylus("empty", done)
+  })
 
   it("should reset", (done) => {
-    runStylus("reset", done);
-  });
+    runStylus("reset", done)
+  })
 
   it("should units", (done) => {
-    runStylus("units", done);
-  });
-});
+    runStylus("units", done)
+  })
+})
---
+++ demo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>Twindy Demo</div>
+  <div class="prose">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import HelloWorld from "./components/HelloWorld.vue"
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+})
+</script>
+
+<style lang="styl">
+@import "../../index"
+
+use inter
+
+.prose
+  use prose
+</style>
