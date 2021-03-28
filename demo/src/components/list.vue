1f7024760b15aca2786691bb1e00f47cffcbf264
Dirk Holtwick <dirk.holtwick@gmail.com>
2021-03-28 12:55:36 +0200

    Sample for new attribute behavior

---
+++ demo/src/components/list.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="list">
+    <div>list vue3 compatible, hiding attrs with null</div>
+    <div class="sample">
+      <template v-for="{ title, active } in values">
+        <a href="#" :active="active || null">{{ title }}</a>
+      </template>
+    </div>
+    <div>list vue2 compatible, hiding attrs with false</div>
+    <div class="sample">
+      <template v-for="{ title, active } in values">
+        <a href="#" :active="active">{{ title }}</a>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const values = [
+  { title: "aaa", active: true },
+  { title: "bbb", active: false },
+  { title: "ccc" },
+]
+</script>
+
+<style lang="styl" scoped>
+@require "twindy"
+
+a
+  display block
+
+  &[active]
+    font-weight semi
+    color -green-800
+</style>
